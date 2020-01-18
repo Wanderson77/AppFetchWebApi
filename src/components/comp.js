797fbc0e6154cce3c146ce2eb5504e19f5235e81
Wanderson Aldo <wanderson.aldo@gmail.com>
2020-01-18 10:23:17 -0200

    Commit base

---
+++ src/components/comp.js
@@ -0,0 +1,81 @@
+/** Comments
+ * 
+ */
+import React, { Component } from 'react';
+import { StyleSheet, View, Button, Alert} from 'react-native';
+ 
+export default class App extends Component {
+ 
+  getDataUsingGet(){
+    //GET request 
+    fetch('https://jsonplaceholder.typicode.com/posts/1', {
+        method: 'GET'
+        //Request Type 
+    })
+    .then((response) => response.json())
+    //If response is in json then in success
+    .then((responseJson) => {
+        //Success 
+        alert(JSON.stringify(responseJson));
+        console.log(responseJson);
+    })
+    //If response is not in json then in error
+    .catch((error) => {
+        //Error 
+        alert(JSON.stringify(error));
+        console.error(error);
+    });
+  }
+ 
+  getDataUsingPost(){
+    //POST json 
+    var dataToSend = {title: 'foo', body: 'bar', userId: 1};
+    //making data to send on server
+    var formBody = [];
+    for (var key in dataToSend) {
+      var encodedKey = encodeURIComponent(key);
+      var encodedValue = encodeURIComponent(dataToSend[key]);
+      formBody.push(encodedKey + "=" + encodedValue);
+    }
+    formBody = formBody.join("&");
+    //POST request 
+    fetch('https://jsonplaceholder.typicode.com/posts', {
+      method: "POST",//Request Type 
+      body: formBody,//post body 
+      headers: {//Header Defination 
+        'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8'
+      },
+    })
+    .then((response) => response.json())
+    //If response is in json then in success
+    .then((responseJson) => {
+        alert(JSON.stringify(responseJson));
+        console.log(responseJson);
+    })
+    //If response is not in json then in error
+    .catch((error) => {
+      alert(JSON.stringify(error));
+      console.error(error);
+    });
+  }  
+  render() {
+
+    return (
+      <View style={styles.MainContainer}>
+        {/*Running GET Request*/}
+        <Button title='Get Data Using GET' onPress={this.getDataUsingGet}/>
+        {/*Running POST Request*/}
+        <Button title='Get Data Using POST' onPress={this.getDataUsingPost}/>
+      </View>
+    );
+  }
+
+}
+
+const styles = StyleSheet.create({
+  MainContainer :{
+    justifyContent: 'center',
+    flex:1,
+    margin: 10
+  }
+});
